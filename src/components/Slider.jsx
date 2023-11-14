b9bfb9cccd77cdbaba66f7936e69d3ace82e5de0
Desarrollo Mercadeo <web.mercadeo@grupo-pb.com>
2023-11-14 16:56:08 -0600

    2, se termino slider, se agrego propuesta section, falta valores de propuesta

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,31 +4,15 @@ import './styles/Slider.scss';
 
 
 // este componente define el contenido del panel, Slider 1 (Hero del Home Page)
-const InfoHeroHome = () => ( 
-      <div className='infoHeroHome-content'>
+const InfoHeroHome = ({ dataSlider }) => ( 
+      <div className='infoHeroHome-content'>     
+          <p>
+            {dataSlider.text1}
+          </p>
 
-        <div className='title-content'>
-          <h1>Tienda de Equipos y Herramientas Automotrices</h1>
-        </div>
-        
-        <div className='parrafo-content'>
           <p>
-            En nuestro espacio, celebramos el apasionante mundo de la mecánica.
-          </p>
-          <p>
-            Descubre la amplia gama de productos que ofrecemos, desde herramientas y repuestos hasta accesorios y equipo de alta calidad.
-            Atendemos la demanda ya sea de entusiastas del automovilismo, mecánicos profesionales o aficionados.
-          </p>
-          <p>
-            <b>
-              ¡Te invitamos a explorar nuestro catálogo y satisfacer tus necesidades de equipo y herramientas con la máxima calidad y variedad, respaldada por más de 23 años de experiencia en la industria!
-            </b>
-          </p>
-          <p>
-            Ofrecemos los mejores precios del mercado y una atención al cliente excepcional. Descubre la mejor calidad en mecánica con nosotros.
-          </p>
-        </div>
-        
+            {dataSlider.text2}
+          </p>       
       </div>
 );
 
@@ -36,13 +20,15 @@ const InfoHeroHome = () => (
 
 const Panel = ({ panelInfo, dataSlider, activeID }) => {
 
-  return (
-    <aside className="panel">
+    const panelClassName = `panel${panelInfo ? `-${panelInfo}` : ''}`;
 
-      {panelInfo === 'heroHome' && <InfoHeroHome />}
+    return (
+        <aside className={panelClassName}>
 
-    </aside>
-  );
+        {panelInfo === 'heroHome' && <InfoHeroHome dataSlider={dataSlider[activeID]} />}
+
+        </aside>
+    );
 
 };
 
@@ -75,7 +61,7 @@ const Slider = ({ dataSlider, panelInfo }) => {
     const interval = setInterval(() => {
       const nextID = (activeID + 1) % dataSlider.length;
       changeActive(nextID);
-    }, 5000); 
+    }, 10000); 
 
     return () => {
       clearInterval(interval); 
@@ -89,7 +75,7 @@ const Slider = ({ dataSlider, panelInfo }) => {
       <Panel dataSlider={dataSlider} panelInfo={panelInfo} activeID={activeID} />
     )}
 
-    {panelInfo !== "heroHome" && <div className='overlay-wrapper'></div>}
+    {panelInfo === "heroHome" && <div className='overlay-wrapper'></div>}
 
     
     </section>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,19 +11,27 @@ const dataHero = [
     {
       id: 0,
       img: 'https://s3.amazonaws.com/gpblogistic.com/Recursos-GPB-Logistic/Home/sliderHome/sliderHome-img1.jpeg',
+      text1: "En GPB Logistic, nacimos de la necesidad. Al darnos cuenta de las limitadas opciones de logística de calidad en Guatemala, nos propusimos redefinir los estándares",
+      text2: "Hoy, no somos simplemente una empresa de transporte de carga; buscamos ser tu socio logístico a largo plazo",
     },
     {
       id: 1,
       img: 'https://s3.amazonaws.com/gpblogistic.com/Recursos-GPB-Logistic/Home/sliderHome/sliderHome-img2.jpeg',
+      text1: "En GPB Logistic, nacimos de la necesidad. Al darnos cuenta de las limitadas opciones de logística de calidad en Guatemala, nos propusimos redefinir los estándares",
+      text2: "Hoy, no somos simplemente una empresa de transporte de carga; buscamos ser tu socio logístico a largo plazo",
     },
     {
       id: 2,
       img: 'https://s3.amazonaws.com/gpblogistic.com/Recursos-GPB-Logistic/Home/sliderHome/sliderHome-img3.jpeg',
+      text1: "Logística sin Contratiempos, Compromiso Incansable en un mundo donde la logística debería ser la última de tus preocupaciones, estamos aquí para asumir la responsabilidad.",
+      text2: "Trabajamos incansablemente, día tras día, para descubrir alternativas innovadoras, comprometidos con la mejora continua tanto en la excelencia del servicio para nuestros clientes como en la calidad de vida de nuestro equipo.",
     },
     {
-        id: 3,
-        img: 'https://s3.amazonaws.com/gpblogistic.com/Recursos-GPB-Logistic/Home/sliderHome/sliderHome-img4.jpeg',
-      },
+      id: 3,
+      img: 'https://s3.amazonaws.com/gpblogistic.com/Recursos-GPB-Logistic/Home/sliderHome/sliderHome-img4.jpeg',
+      text1: "Logística sin Contratiempos, Compromiso Incansable en un mundo donde la logística debería ser la última de tus preocupaciones, estamos aquí para asumir la responsabilidad.",
+      text2: "Trabajamos incansablemente, día tras día, para descubrir alternativas innovadoras, comprometidos con la mejora continua tanto en la excelencia del servicio para nuestros clientes como en la calidad de vida de nuestro equipo.",
+    },
   ];
 
 
@@ -47,6 +55,40 @@ const Home = () => {
                     <Slider dataSlider={dataHero} panelInfo='heroHome' />
                 </div>
             </section>
+
+            <section className='propuesta-section'>
+                <article className='title'>
+                    <h1>PROPUESTAS DE VALOR</h1>
+                </article>
+
+                <aside className='propuesta-space'>
+                    <div className='valoresOverlay-space'>
+                        <div className='valores-space'>
+                            <div className='valor-item'>
+
+                            </div>
+
+                            <div className='valor-item'>
+
+                            </div>
+
+                            <div className='valor-item'>
+
+                            </div>
+
+                            <div className='valor-item'>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <img src="https://s3.amazonaws.com/gpblogistic.com/Recursos-GPB-Logistic/Home/propuesta/propuesta-joven-img.png" alt="" className='persona-img'/>
+                </aside>
+            </section>
+
+            <div className='space-margin'>
+
+            </div>
         </main>
 
         <footer>
